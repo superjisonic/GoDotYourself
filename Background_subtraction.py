dd17f02a93f53e934c8f782eaf12fd5870cc6d36
rbqbwbrb <rbqbwbrb@daum.net>
2019-05-24 01:21:56 +0900

    ctrl + 마우스왼쪽키 더블클릭 : 배경 색 지정 a : 자동 배경제거 s : 저장 o : 저장한 이미지 띄우기 n : 이미지에 드로우한 후 배경 업데이트

--- Background_subtraction.py
+++ Background_subtraction.py
@@ -18,55 +18,72 @@ import numpy as np
 import cv2
 import sys
 
-faceCascade = cv2.CascadeClassifier(
-    './haarcascades/haarcascade_frontalface_default.xml')
-
 
 def draw_detections(img, rects, thickness=1):
     for x, y, w, h in rects:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness)
 
 
-def check_border(img, x, y, w, h):
+def check_border(img, x, y, w, h): # Check to adjust if it crosses the boundary
     if x < 0:
         ret_x = 0
     else:
         ret_x = x
-    if x + w > img.shape[1]:
-        ret_w = img.shape[1] - x - 1
+    if ret_x + w > img.shape[1] - 1:
+        ret_w = img.shape[1] - ret_x - 1
     else:
         ret_w = w
     if y < 0:
         ret_y = 0
     else:
         ret_y = y
-    if y + h > img.shape[0]:
-        ret_h = img.shape[0] - y - 1
+    if ret_y + h > img.shape[0] - 1:
+        ret_h = img.shape[0] - ret_y - 1
     else:
         ret_h = h
     return (ret_x, ret_y, ret_w, ret_h)
 
 
-def inside(r, q):  # if r is in q : to overlap
+def inside(r, q):  # if r is in q : to prevent overlap
     rx, ry, rw, rh = r
     qx, qy, qw, qh = q
     return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
 
-
-def grabcut(img, mask, faces):
+def grabcut(img, mask, faces): # grabcut with multiple faces
     pad_x = faces[0][0]
     pad_y = faces[0][1]
     pad_w = faces[0][2]
     pad_h = faces[0][3]
     for x, y, w, h in faces:
-        if x < pad_x: pad_x = x
-        if y < pad_y: pad_y = y
-        if x + w > pad_x + pad_w: pad_w = w
-        if y + h > pad_y + pad_h: pad_h = h
+        if x < pad_x:
+            if x + w < pad_x + pad_w:
+                pad_w = pad_x - x + pad_w
+                pad_x = x
+            else:
+                pad_w = w
+                pad_x = x
+        else:
+            if x + w < pad_x + pad_w:
+                pass
+            else:
+                pad_w = x - pad_x + w
+
+        if y < pad_y:
+            if y + h < pad_y + pad_h:
+                pad_h = pad_y - y + pad_h
+                pad_y = y
+            else:
+                pad_h = h
+                pad_y = y
+        else:
+            if y + h < pad_y + pad_h:
+                pass
+            else:
+                pad_h = y - pad_y + h
     bgdmodel = np.zeros((1, 65), np.float64)
     fgdmodel = np.zeros((1, 65), np.float64)
     cv2.grabCut(img, mask, check_border(img, pad_x - pad_w, pad_y - pad_h, 2 * pad_w, 3 * pad_h), bgdmodel, fgdmodel, 1,
-                cv2.GC_INIT_WITH_RECT)  # appropriate value
+cv2.GC_INIT_WITH_RECT)  # appropriate value
 
 
 class BGsubtractor():
@@ -83,7 +100,7 @@ class BGsubtractor():
 
     # setting up flags
     rect = (0, 0, 1, 1)
-    bg_color = BLACK
+    bg_color = BLACK # default background color
     drawing = False  # flag for drawing curves
     rectangle = False  # flag for drawing rect
     rect_over = False  # flag to check if rect drawn
@@ -96,7 +113,7 @@ class BGsubtractor():
         # to extract background color
         if event == cv2.EVENT_LBUTTONDBLCLK:
             if flags & cv2.EVENT_FLAG_CTRLKEY:
-                self.bg_color = self.img[y, x]
+                self.bg_color = self.img2[y, x]
                 print(self.bg_color)
 
         # Draw Rectangle
@@ -120,14 +137,13 @@ class BGsubtractor():
             print(" Now press the key 'n' a few times until no further change \n")
 
         # draw touchup curves
-
         if event == cv2.EVENT_LBUTTONDOWN:
             if self.rect_over == False:
                 print("first draw rectangle or press 'a' key\n")
             else:
                 self.drawing = True
-                cv2.circle(self.img, (x, y), self.thickness, self.value['color'], -1)  # img에는 색깔을 입힘
-                cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)  # mask 에 값을 0~3중 하나를 줌
+                cv2.circle(self.img, (x, y), self.thickness, self.value['color'], -1)
+                cv2.circle(self.mask, (x, y), self.thickness, self.value['val'], -1)
 
         elif event == cv2.EVENT_MOUSEMOVE:
             if self.drawing == True:
@@ -148,6 +164,9 @@ class BGsubtractor():
             print("No input image given, so loading default image, lena.jpg \n")
             filename = 'lena.jpg'
 
+        faceCascade = cv2.CascadeClassifier( # for recognizing faces
+            './haarcascades/haarcascade_frontalface_default.xml')
+
         self.img = cv2.imread(filename)
         # self.img = cv2.resize(self.img, dsize=(512,512), interpolation=cv2.INTER_LINEAR) #to resize the image
         self.img2 = self.img.copy()  # a copy of original image
@@ -167,7 +186,7 @@ class BGsubtractor():
 
         while (1):
 
-            cv2.imshow('output', self.output)
+            #cv2.imshow('output', self.output)
             cv2.imshow('input', self.img)
 
             k = cv2.waitKey(1)
@@ -188,18 +207,8 @@ class BGsubtractor():
             elif k == ord('o'):
                 filename = './data/dst.jpg'
                 result = cv2.imread(filename)
-                # to remove noise
-                # gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-                # ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                # kernel = np.ones((3, 3), np.uint8)
-                # closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=5)
-                # opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel, iterations=5)
                 cv2.imshow('result', result)
             elif k == ord('s'):  # save image
-                bar = np.zeros((self.img.shape[0], 5, 3), np.uint8)
-                res = np.hstack((self.img, bar, self.output))
-                cv2.imwrite('./data/result.png', res)
-                cv2.imwrite('./data/dst1.png', self.output)
                 cv2.imwrite('./data/dst.jpg', bg_result)
                 print(" Result saved as image \n")
             elif k == ord('r'):  # reset everything
@@ -228,7 +237,7 @@ class BGsubtractor():
                         else:
                             found_filtered.append(r)
                     grabcut(self.img2, self.mask, found_filtered)
-                    # draw_detections(self.img, found_filtered, 3)
+                    #draw_detections(self.img, found_filtered, 3)
                 except IndexError:
                     self.auto = False
                     self.rect_over = False
@@ -239,7 +248,7 @@ class BGsubtractor():
                 print(""" For finer touchups, mark foreground and background after pressing keys 0-3
                 and again press 'n' \n""")
                 try:
-                    if self.rect_or_mask == 0:
+                    if self.rect_or_mask == 0:  # grabcut with rect
                         bgdmodel = np.zeros((1, 65), np.float64)
                         fgdmodel = np.zeros((1, 65), np.float64)
                         cv2.grabCut(self.img2, self.mask, self.rect, bgdmodel, fgdmodel, 1, cv2.GC_INIT_WITH_RECT)
@@ -257,14 +266,14 @@ class BGsubtractor():
 
             mask2 = np.where((self.mask == 1) + (self.mask == 3), 255, 0).astype('uint8')  # foreground mask
             mask3 = np.where((self.mask == 0) + (self.mask == 2), 255, 0).astype('uint8')  # background mask
-
+            # output without background
             self.output = cv2.bitwise_and(self.img2, self.img2, mask=mask2)
             # output with selected background color
             bg = np.full((self.img.shape[0], self.img.shape[1], 3), fill_value=self.bg_color, dtype=np.uint8)  #
             bg = cv2.bitwise_and(bg, bg, mask=mask3)  #
             bg_result = self.output + bg
 
-            cv2.imshow('bg', bg_result)
+            cv2.imshow('output', bg_result)
             # smoothing
             # gray = cv2.cvtColor(self.output, cv2.COLOR_BGR2GRAY)
             # ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
