dbc1d91b6a0959d501053c152f77edd8609d0af0
yesoly <47820809+yesoly@users.noreply.github.com>
2019-05-24 17:52:25 +0900

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("yesol.jpg")   #내사진
+img.shape
+
+b, g, r = cv2.split(img)
+img = cv2.merge([r,g,b])
+
+img_resized = cv2.resize(img, dsize=(30 , 40)) #3:4 비율
+cv2.imwrite('change.jpg', img_resized)
+
+plt.subplot(221)
+plt.imshow(img, cmap=plt.cm.gray)
+plt.title("DoDotYourSelf Upload Image")
+plt.axis("off")
+
+plt.subplot(222)
+plt.imshow(img_resized, cmap=plt.cm.gray)
+plt.title("DoDotYourSelf Change Image")
+plt.axis("off")
+
+plt.show()
---
+++ main.py
@@ -0,0 +1,59 @@
+#############################
+# Python Image Pixelator    #
+# openCV library            #
+#############################
+
+from PIL import Image
+import cv2
+
+def fill(op, pos, n, avg):
+    # Fills the new image pixels with the average values
+    y, x = pos
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            op.putpixel((i, j), avg)
+
+
+# Calculates average RGB values of the n*n pixel matrix
+def pixel_avg(square, n):
+    r, g, b = 0, 0, 0
+    for x in range(n):
+        for y in range(n):
+            r += square[x][y][2]
+            g += square[x][y][1]
+            b += square[x][y][0]
+    r, g, b = r // (n * n), g // (n * n), b // (n * n)
+    return (r, g, b)
+
+
+def main():
+    name = 'yesol.jpg'  # give your filename here
+    pixels = cv2.imread(name)  # img 역할
+
+    im_height, im_width = pixels.shape[:2]
+
+    # Tweak this parameter
+    ######
+    n = 15  # This is the pixelation factor
+    ######
+
+    # These pixels are ignored for now
+    rem_width = im_width % n
+    rem_height = im_height % n
+    print(im_height, im_width)
+
+    # The ouput image
+    # Red pixels in the end result denote the unprocessed pixels
+    op = Image.new('RGB', (im_width, im_height), color='red')
+
+    for x in range(0, im_height - rem_height, n):
+        for y in range(0, im_width - rem_width, n):
+            avg = pixel_avg(pixels[x:x + n, y:y + n], n)
+            fill(op, (x, y), n, avg)
+
+    op.show()
+    op.save(name.split('.')[0] + '_pixel.jpg')
+
+
+if __name__ == '__main__':
+    main()
